597c857e6ba30a58108228332622b05b7ba56e69
Park Sanghyun <p981116@naver.com>
2021-09-05 17:04:16 +0900

    solving by powerful104

---
+++ Weekly Challenge/2주차_상호 평가.py
@@ -0,0 +1,27 @@
+def solution(scores):
+    answer = ''
+    scores = [list(x) for x in zip(*scores)]
+    answer = ''
+    for i in range(len(scores)):
+        selfs = scores[i][i]
+        if (selfs == max(scores[i]) or selfs == min(scores[i])) and scores[i].count(selfs) == 1:
+            scores[i][i] = -1
+    for i in range(len(scores)):
+        n = len(scores[i])
+        su = sum(scores[i])
+        if scores[i][i] == -1:
+            n -=1
+            su +=1
+        mean = su/n
+        print(mean)
+        if mean >= 90:
+            answer += 'A'
+        elif mean >= 80:
+            answer += 'B'
+        elif mean >= 70:
+            answer += 'C'
+        elif mean >= 50:
+            answer += 'D'
+        else:
+            answer += 'F'
+    return answer
