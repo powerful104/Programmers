e4466ea45b0293d1cfa29382161375df58e9302a
Park Sanghyun <p981116@naver.com>
2021-09-06 15:29:25 +0900

    solved by powerful104

--- Weekly Challenge/5주차_모음 사전.py
+++ Weekly Challenge/5주차_모음 사전.py
@@ -1,2 +1,10 @@
-word = ''
-answer = 0
+def solution(word):
+    wordN = {'A':0,'E':1,'I':2,'O':3,'U':4}
+    word = list(word)
+    org = 5**5
+    answer = 0
+    for i in word:
+        answer += wordN[i]*(org-1)//4
+        org //= 5
+    answer += len(word)
+    return answer
