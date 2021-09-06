b8d701b9a988889d4318e3c64a756ec54cafbf61
Park Sanghyun <p981116@naver.com>
2021-09-06 16:11:27 +0900

    solved by powerful104

--- Level 2/124 나라의 숫자.py
+++ Level 2/124 나라의 숫자.py
@@ -1,21 +1,20 @@
-n = 13
+def solution(n):
+    word = ['1', '2', '4']
+    length = 1
+    org = 3
 
-word = ['1', '2', '4']
-length = 1
-org = 3
+    while(org < n):
+        n -= org
+        length+=1
+        org*=3
 
-while(org < n):
-    n -= org
-    length+=1
-    org*=3
-
-answer = ''
-tmp = 3**(length-1)
-for i in range(length):
-    ite = 0
-    while(tmp < n):
-        n -= tmp
-        ite += 1
-    tmp //= 3
-    answer+=word[ite]
-print(answer)
+    answer = ''
+    tmp = 3**(length-1)
+    for i in range(length):
+        ite = 0
+        while(tmp < n):
+            n -= tmp
+            ite += 1
+        tmp //= 3
+        answer+=word[ite]
+    return answer
