dbd3167227f6aea2ff015e329e4f6d29fa0bfc4e
Park Sanghyun <p981116@naver.com>
2021-09-15 18:16:27 +0900

    solved by powerful104

--- Level 2/후보키.py
+++ Level 2/후보키.py
@@ -1,31 +1,35 @@
 from itertools import combinations
-relation = [["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]
-check = []
-for i in range(len(relation[0])):
-    li = list(combinations(range(len(relation[0])),i+1))
-    for j in li:
-        tmpRela = []
-        for k in relation:
-            tmp = []
-            for l in j:
-                tmp.append(k[l])
-            tmpRela.append(tuple(tmp))
-        tmpSet = set(tmpRela)
-        if len(tmpRela) == len(tmpSet):
-            check.append(list(j))
-cur = 0
-while True:
-    cur2 = cur
+def solution(relation):
+    check = []
+    for i in range(len(relation[0])):
+        li = list(combinations(range(len(relation[0])),i+1))
+        for j in li:
+            tmpRela = []
+            for k in relation:
+                tmp = []
+                for l in j:
+                    tmp.append(k[l])
+                tmpRela.append(tuple(tmp))
+            tmpSet = set(tmpRela)
+            if len(tmpRela) == len(tmpSet):
+                check.append(list(j))
+    cur = 0
     while True:
-        cur2 += 1
-        print(cur, cur2)
-        if cur2 == len(check):
+        cur2 = cur
+        while True:
+            cur2 += 1
+            if cur2 == len(check):
+                break
+            if len(list(set(check[cur])-(set(check[cur2]) & set(check[cur])))) == 0:
+                del check[cur2]
+                cur2 -= 1
+        cur += 1
+        if cur == len(check):
             break
-        print(set(check[cur2]) | set(check[cur]))
-        if len(list(set(check[cur])-(set(check[cur2]) | set(check[cur])))) == 0:
-            del check[cur2]
-            cur2 -= 1
-    cur += 1
-    if cur == len(check):
-        break
-print(check)
+    return len(check)
+"""
+가능한 모든 키의 조합을 나타낸후 해당 키의 조합에서 생기는 relation의 조합을 모두 뽑아낸후
+첫번째 조건인 유일성을 만족하는지 확인후 유일성을 만족하는 키의 조합만을 남긴다
+그후 키의 조합의 최소성을 만족시키기 위해서 set를 이용 중복되는값이 있다면 가장 길이가 적은 것만을 남기는 식으로 정리한다.
+정답은 맞췄지만 너무 복잡하게 푼 느낌이다
+"""
