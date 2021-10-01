bfeeb98fe570f7e230af0ad854e1ff24b37a5b87
Park Sanghyun <p981116@naver.com>
2021-10-01 14:46:35 +0900

    solved by powerful104

--- Level 2/순위 검색.py
+++ Level 2/순위 검색.py
@@ -1,3 +1,4 @@
+import bisect
 
 def solution(info, query):
     language = {'cpp' : 0, 'java' : 1, 'python' : 2, '-' : -1}
@@ -14,7 +15,7 @@ def solution(info, query):
         j = job[information[1]]
         c = career[information[2]]
         s = soulfood[information[3]]
-        DB[l][j][c][s].append(information[4])
+        DB[l][j][c][s].insert(bisect.bisect_left(DB[l][j][c][s], int(information[4])),int(information[4]))
     for q in query:
         ans = 0
         q = q.split()
@@ -27,7 +28,7 @@ def solution(info, query):
             tmpl = list(range(len(DB)))
         else:
             tmpl.append(l)
-        for i in range(len(DB)):
+        for i in range(3):
             if not i in tmpl:
                 continue
             tmpj = []
@@ -35,7 +36,7 @@ def solution(info, query):
                 tmpj = list(range(len(DB[0])))
             else:
                 tmpj.append(j)
-            for ii in range(len(DB[i])):
+            for ii in range(2):
                 if not ii in tmpj:
                     continue
                 tmpc = []
@@ -43,7 +44,7 @@ def solution(info, query):
                     tmpc = list(range(len(DB[0][0])))
                 else:
                     tmpc.append(c)
-                for iii in range(len(DB[i][ii])):
+                for iii in range(2):
                     if not iii in tmpc:
                         continue
                     tmps = []
@@ -51,20 +52,13 @@ def solution(info, query):
                         tmps = list(range(len(DB[0][0][0])))
                     else:
                         tmps.append(s)
-                    for iiii in range(len(DB[i][ii][iii])):
+                    for iiii in range(2):
                         if not iiii in tmps:
                             continue
-                        for num in DB[i][ii][iii][iiii]:
-                            if int(num) >= int(q[7]):
-                                ans += 1
+                        ans += len(DB[i][ii][iii][iiii]) - bisect.bisect_left(DB[i][ii][iii][iiii],int(q[7]))
         answer.append(ans)
 
     return answer
-
-info =["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"]
-query = ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]
-
-print(solution(info,query))
 
 """
 배열을 이용하여 빠르게 접근 하려 했지만 실패
@@ -72,4 +66,8 @@ print(solution(info,query))
 만약 "-"일 때는 전체를 참조하도록 4중 for문을 설치하면 될지도 모른다.
 
 + 해당 방법을 이용하여 풀이를 진행하여 정확도는 통과하였지만 효율성에서 불통하였다.
+
++ DB에 값을 집어넣는 과정에서 이진탐색을 이용하여 값을 정렬하며 집어넣었고
+내가 원하는 값보다 큰수들을 찾을때에도 이진탐색을 이용 해당 인덱스를 전체 개수에서 빼는식으로 큰수의 개수를 찾았다.
+
 """
