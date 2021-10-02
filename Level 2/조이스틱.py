f984884473c387ae8bd8ad5b50349aeab6145326
Park Sanghyun <p981116@naver.com>
2021-10-02 21:03:06 +0900

    solved by powerful104

--- Level 2/조이스틱.py
+++ Level 2/조이스틱.py
@@ -1,15 +1,31 @@
-name = "JEROEN"
-unco = [i for i in range(len(name)) if name[i] != 'A' ]
-print(unco)
-cur = 0
-answer = 0
-if unco[0] >= len(name)-unco[-1] + cur:
-    cur = unco[0]
-    answer += unco[0]
-else:
-    cur = unco[-1]
-    answer += len(name)-unco[-1] + cur
+def solution(name):
+    co = [int(i != "A") for i in name]
+    cur = 0
+    answer = 0
+    while co.count(1) != 0:
+        cor = list(reversed(co+co[:cur])).index(1)
+        col = co[cur:]
+        ans = cor + 1
+        if col.count(1) == 0:
+            cur -= ans
+            if cur<0:
+                cur += len(name)
+        else:
+            col = col.index(1)
+            if cor + 1 < col:
+                cur -= ans
+                if cur<0:
+                    cur += len(name)
+            else:
+                ans = col
+                cur += col
+        co[cur] = 0
+        answer += ans + min(91-ord(name[cur]),ord(name[cur])-65)
+    return answer
 
 """
+먼저 가로로 어떻게 이동해야 하는가를 고려하였고, 가로의 이동 방향에 따라 직접 이동하였다.
+하지만 오른쪽으로 넘어가는 경우는 제외
 
+그러고 A가 아닌 위치만을 cur가 방문 하게 되므로 이를 이용 해당 알파벳이 되려면 몇번 최소 동작해야하는지 구하였다.
 """
