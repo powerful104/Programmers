319c3f9b831bb6bdd54304044d746fa9a6ec1e03
Park Sanghyun <p981116@naver.com>
2022-05-11 23:34:56 +0900

    solved by powerful104

--- Level 1/신고 결과 받기.py
+++ Level 1/신고 결과 받기.py
@@ -1,16 +1,26 @@
 def solution(id_list, report, k):
     dict = {string : i for i,string in enumerate(id_list)}
-    li = [[] for _ in range(len(id_list))]
+    report_list = [[0 for _ in range(len(id_list))] for _ in range(len(id_list))]
+    ban_list = []
+    answer = []
+    
     for i in report:
         name, report_name = i.split()
-        li[dict[name]].append(report_name)
+        report_list[dict[report_name]][dict[name]] = 1
     
-    for i in li:
-        i = set(i)
-        print(i)
+    for i in range(len(report_list)):
+        if sum(report_list[i]) >= k:
+            ban_list.append(i)
     
-    answer = []
+    for i in range(len(id_list)):
+        temp = 0
+        for j in ban_list:
+            temp += report_list[j][i]
+        answer.append(temp)
+    
     return answer
 
-
-solution(["muzi", "frodo", "apeach", "neo"],["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],2)
+    """
+    너무 복잡하게 짠듯한 감이 있는 코드다.
+    나중에 수정이 필요해 보인다.
+    """
