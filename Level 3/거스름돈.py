f2d623899e5eef35fd0b7f0d969d966d0b2d5281
powerful104 <p981116@naver.com>
2022-05-25 01:24:16 +0900

    solved by powerful104

---
+++ Level 3/거스름돈.py
@@ -0,0 +1,15 @@
+def solution(n, money):
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for coin in money:
+        for price in range(coin, n+1):
+            dp[price] += dp[price - coin]
+    return dp[n] % 1000000007
+
+    """
+    처음 거스름돈을 보고 간단하게 생각했다.
+    하지만 내가 간단하게 생각한문제는 최소 거스름돈 개수를 찾는 문제 였고
+    해당 문제는 접근법이 달랐다.
+    
+    다른사람의 풀이를 참고하여 작성하였다.
+    """
